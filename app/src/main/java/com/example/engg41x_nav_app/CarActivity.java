87d00e2ff3f29cdddeede9f4ef5b728e54c19abc
Kade BC <kade.w.bc@gmail.com>
2024-02-08 16:49:26 -0500

    add secondary page

---
+++ app/src/main/java/com/example/engg41x_nav_app/CarActivity.java
@@ -0,0 +1,14 @@
+package com.example.engg41x_nav_app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the layout for CarActivity
+        setContentView(R.layout.activity_car);
+    }
+}
--- app/src/main/java/com/example/engg41x_nav_app/MapsActivity.java
+++ app/src/main/java/com/example/engg41x_nav_app/MapsActivity.java
@@ -2,7 +2,10 @@ package com.example.engg41x_nav_app;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,10 +27,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        Button mapActionButton = findViewById(R.id.button_map_action);
+        mapActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start CarActivity
+                Intent intent = new Intent(MapsActivity.this, CarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("HERE WE ARE");
     }
 
     @Override
