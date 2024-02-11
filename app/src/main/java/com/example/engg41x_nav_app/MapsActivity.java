d4736fb24d95becfb7c4fe77323d5a529a8f8b24
Kade BC <kade.w.bc@gmail.com>
2024-02-11 16:22:51 -0500

    get two points working

--- app/src/main/java/com/example/engg41x_nav_app/MapsActivity.java
+++ app/src/main/java/com/example/engg41x_nav_app/MapsActivity.java
@@ -3,17 +3,36 @@ package com.example.engg41x_nav_app;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.engg41x_nav_app.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,6 +61,17 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+        Button getDirectionsButton = findViewById(R.id.btn_get_directions);
+        getDirectionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText destinationInput = findViewById(R.id.destination_input);
+                String destination = destinationInput.getText().toString();
+                //fetchDirections("your_origin_lat,your_origin_lng", destination);
+                fetchDirections("43.53007,-80.22566", "43.51807, -80.23904");
+            }
+        });
+
     }
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -51,5 +81,103 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(43.53007, -80.22566);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker at UofG"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+    private List<LatLng> decodePoly(String encoded) {
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0, len = encoded.length();
+        int lat = 0, lng = 0;
+
+        while (index < len) {
+            int b, shift = 0, result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lat += dlat;
+
+            shift = 0;
+            result = 0;
+            do {
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5;
+            } while (b >= 0x20);
+            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
+            lng += dlng;
+
+            LatLng p = new LatLng((((double) lat / 1E5)),
+                    (((double) lng / 1E5)));
+            poly.add(p);
+        }
+
+        return poly;
+    }
+
+    private void drawPolylineOnMap(List<LatLng> list) {
+        PolylineOptions options = new PolylineOptions().width(10).color(Color.BLUE).geodesic(true);
+        options.addAll(list);
+        mMap.addPolyline(options);
+
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        for (LatLng latLng : list) {
+            builder.include(latLng);
+        }
+        LatLngBounds bounds = builder.build();
+
+        int padding = 100;
+        CameraUpdate cu = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        mMap.animateCamera(cu);
+    }
+
+    private void fetchDirections(String origin, String destination) {
+        new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... strings) {
+                String origin = strings[0];
+                String destination = strings[1];
+                String response = "";
+                try {
+                    String urlString = "https://maps.googleapis.com/maps/api/directions/json?" +
+                            "origin=" + origin + "&destination=" + destination + "&key=AIzaSyBBoCNH1FTP-sVY1FCvAHyM8uur8-FP5CU";
+                    URL url = new URL(urlString);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    try {
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line).append('\n');
+                        }
+                        response = sb.toString();
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return response;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                try {
+                    // Parse the JSON response
+                    final JSONObject json = new JSONObject(result);
+                    JSONArray routes = json.getJSONArray("routes");
+                    JSONObject route = routes.getJSONObject(0);
+                    JSONObject polyline = route.getJSONObject("overview_polyline");
+                    String encodedPolyline = polyline.getString("points");
+                    List<LatLng> list = decodePoly(encodedPolyline);
+                    // Draw the polyline on the map
+                    runOnUiThread(() -> drawPolylineOnMap(list));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.execute(origin, destination);
     }
 }
